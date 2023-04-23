224c4d3a2b3d29e2e56ba6b1b1b32861df4e75a6
jeffreyheideman <jeffrey_heideman@hotmail.com>
2023-04-23 15:17:50 +0200

    Een begin gemaakt aan de opdracht

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+const countryList = document.getElementById("country-list");
+const errorMessage = document.getElementById("error-message")
+
+async function fetchCountries() {
+    try {
+        const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,flag,population,region")
+        console.log(response.data)
+
+        countryList.innerHTML = `${ response.data[0]}`
+
+    } catch ( e ) {
+        console.error( e )
+        if (e.response.status  === 404) {
+            errorMessage.innerText = "Page not found | 404"
+        } else if (e.response.status === 500) {
+            errorMessage.innerText = "internal server error | 500"
+        }
+    }
+}
+
+void fetchCountries()
